abb90d230e966e714ebe11b8f28d3ecf60f55f68
levesque <misteurkill@hotmail.fr>
2022-04-29 12:47:24 +0200

    feat: create algo and tested it on each cases

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const fizzBuzzEmitter = new MyEmitter();
 
 const eventName = 'FizzBuzz';
 
-function isDivibleBy3And5(nbr) {
-  return nbr % 3 == 0 && nbr % 5 == 0;
-}
-
-fizzBuzzEmitter.on(eventName, function (nbr1) {});
-fizzBuzzEmitter.emit(eventName, 5);
+fizzBuzzEmitter.on(eventName, function (nbr) {
+  if (nbr % 5 == 0) {
+    console.log('Buzz');
+  } else if (nbr % 3 == 0) {
+    console.log('Fizz');
+  } else {
+    console.log("doesn't pass number : ", nbr);
+  }
+});
+fizzBuzzEmitter.emit(eventName, 10);
+fizzBuzzEmitter.emit(eventName, 6);
+fizzBuzzEmitter.emit(eventName, 7);
