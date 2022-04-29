7e33febb46b45d8dad54f18e5c079fe84eaa8955
levesque <misteurkill@hotmail.fr>
2022-04-29 12:34:35 +0200

    feat: set up event and check if event is triggered

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const EventEmitter = require('events');
 
 class MyEmitter extends EventEmitter {}
 const fizzBuzzEmitter = new MyEmitter();
+
+const eventName = 'FizzBuzz';
+
+fizzBuzzEmitter.on(eventName, function (nbr1, nbr2) {
+  console.log(`his called nbr1: ${nbr1} nbr2: ${nbr2}`);
+});
+fizzBuzzEmitter.emit(eventName, 5, 12);
