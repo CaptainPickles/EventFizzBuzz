e21edf13d9cedabd5f9391dd94f02e14a5da1804
levesque <misteurkill@hotmail.fr>
2022-04-29 12:42:23 +0200

    refactor : remove console log

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ const eventName = 'FizzBuzz';
 function isDivibleBy3And5(nbr) {
   return nbr % 3 == 0 && nbr % 5 == 0;
 }
-console.log(
-  'test on 15 : ',
-  isDivibleBy3And5(15),
-  ' test on 5 : ',
-  isDivibleBy3And5(5)
-);
 
-fizzBuzzEmitter.on(eventName, function (nbr1) {
-  console.log(`his called nbr1: ${nbr1}`);
-});
+fizzBuzzEmitter.on(eventName, function (nbr1) {});
 fizzBuzzEmitter.emit(eventName, 5);
