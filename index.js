4d7eb976ad781e1babc5ebb30fed70b12b786d7e
levesque <misteurkill@hotmail.fr>
2022-04-29 12:41:27 +0200

    feat: adding isDivisible function and tested it

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const fizzBuzzEmitter = new MyEmitter();
 
 const eventName = 'FizzBuzz';
 
-fizzBuzzEmitter.on(eventName, function (nbr1, nbr2) {
-  console.log(`his called nbr1: ${nbr1} nbr2: ${nbr2}`);
+function isDivibleBy3And5(nbr) {
+  return nbr % 3 == 0 && nbr % 5 == 0;
+}
+console.log(
+  'test on 15 : ',
+  isDivibleBy3And5(15),
+  ' test on 5 : ',
+  isDivibleBy3And5(5)
+);
+
+fizzBuzzEmitter.on(eventName, function (nbr1) {
+  console.log(`his called nbr1: ${nbr1}`);
 });
-fizzBuzzEmitter.emit(eventName, 5, 12);
+fizzBuzzEmitter.emit(eventName, 5);
